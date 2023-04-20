26b97a55d252ae57b291bc3f7da746a8426cfbe8
Opilane TTHK <78085382+Luikss@users.noreply.github.com>
2023-04-20 14:14:53 +0300

    Home page and app window size

--- CocktailApp/CocktailApp/App.xaml.cs
+++ CocktailApp/CocktailApp/App.xaml.cs
@@ -1,11 +1,34 @@
-﻿namespace CocktailApp;
+﻿using Microsoft.Maui.Handlers;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Views;
+using Windows.Graphics;
+using WinRT.Interop;
+
+namespace CocktailApp;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    const int WindowWidth = 750;
+    const int WindowHeight = 1500;
 
-		MainPage = new AppShell();
-	}
+    public App()
+    {
+        InitializeComponent();
+
+#if WINDOWS
+        WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+        });
+#endif
+
+        MainPage = new NavigationPage(new HomePage());
+    }
 }
---
+++ CocktailApp/CocktailApp/Views/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Views;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
