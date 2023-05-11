2492bfa848b7b28c507b982564b486b9460197cb
Opilane TTHK <andreluige@hot.ee>
2023-05-11 13:56:14 +0300

    Removing unused font

--- CocktailApp/CocktailApp/MauiProgram.cs
+++ CocktailApp/CocktailApp/MauiProgram.cs
@@ -10,7 +10,6 @@ public static class MauiProgram
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("Nunito-Bold.ttf", "NunitoBold");
-				fonts.AddFont("Nunito-Italic.ttf", "NunitoItalic");
 				fonts.AddFont("Nunito-Light.ttf", "NunitoLight");
 				fonts.AddFont("Nunito-Medium.ttf", "NunitoMedium");
 				fonts.AddFont("Nunito-Regular.ttf", "NunitoRegular");
